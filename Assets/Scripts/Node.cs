231870b1ad5507ca18c4b0491d017e58aa69bfd3
Ben Toepke <Ben Toepke>
2016-10-16 10:37:30 -0500

    Nodes WIP

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public int id;
+    public GameObject[] connectionNodes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class NodeConnection
+{
+    public float distance;
+    public GameObject node1;
+    public GameObject node2;
+}
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+
+    GameObject[] nodes;
+
+	// Use this for initialization
+	void Start () {
+        nodes = GameObject.FindGameObjectsWithTag(Tags.Node);
+        for (int i=0; i < nodes.Length; i++)
+        {
+            nodes[i].GetComponent<Node>().id = i + 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
