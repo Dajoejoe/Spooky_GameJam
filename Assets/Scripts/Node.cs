5461719e71e7c2389d4d5d02946c917db982b6c6
Ben Toepke <Ben Toepke>
2016-10-16 11:38:36 -0500

    WIP

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -21,6 +21,34 @@ public class Node : MonoBehaviour {
 public class NodeConnection
 {
     public float distance;
-    public GameObject node1;
-    public GameObject node2;
+    public Node node1;
+    public Node node2;
+
+    public NodeConnection(Node n1, Node n2)
+    {
+        node1 = n1;
+        node2 = n2;
+        distance = Vector3.Distance(n1.transform.position, n1.transform.position);
+    }
+
+    public bool Equals(int id1, int id2)
+    {
+        var n1 = node1.GetComponent<Node>().id;
+        var n2 = node2.GetComponent<Node>().id;
+
+        return (id1 == n1 && id2 == n2) || (id1 == n2 && id2 == n1);
+    }
+
+    public Node OtherNode(Node n1)
+    {
+        if (n1 == node1)
+            return node2;
+        else
+            return node1;
+    }
+
+    public bool ContainsNode(Node n)
+    {
+        return n == node1 || n == node2;
+    }
 }
--- Assets/Scripts/NodeManager.cs
+++ Assets/Scripts/NodeManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class NodeManager : MonoBehaviour {
 
     GameObject[] nodes;
+
+    List<NodeConnection> connections;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +16,93 @@ public class NodeManager : MonoBehaviour {
         {
             nodes[i].GetComponent<Node>().id = i + 1;
         }
+        CreateConnections();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void CreateConnections()
+    {
+        connections = new List<NodeConnection>();
+        foreach (var node in nodes)
+        {
+            var node1 = node.GetComponent<Node>(); 
+            foreach (var connection in node.GetComponent<Node>().connectionNodes)
+            {
+                var node2 = connection.GetComponent<Node>();
+                if (!connections.Any(x => x.Equals(node1.id, node2.id)))
+                {
+                    connections.Add(new NodeConnection(node1, node2));
+                }
+            }
+        }
+    }
+}
+
+public class PathFinder
+{
+    List<NodeConnection> connections;
+    
+
+    public PathFinder(List<NodeConnection> connections)
+    {
+        this.connections = connections;
+    }
+
+    //public List<NodeConnection> FindPath(Node from, Node to)
+    //{
+    //    List<List<NodeConnection>> paths = new List<List<NodeConnection>>();
+    //    foreach (var connection in connections.Where(c => c.node1 == from || c.node2 == from))
+    //    {
+    //        var path = DistanceToConnection(from, connection, to, new List<NodeConnection>());
+    //        if (path == null || path.Count < 2)
+    //        {
+    //            continue;
+    //        }
+    //        paths.Add(path);
+    //    }
+        
+    //    float min = Mathf.Infinity;
+    //    List<NodeConnection> shortest = null;
+    //    foreach (var path in paths)
+    //    {
+    //        float distance = 0;
+    //        foreach (var connection in path)
+    //        {
+    //            distance += connection.distance;
+    //        }
+    //        if (distance < min)
+    //        {
+    //            min = distance;
+    //            shortest = path;
+    //        }
+    //    }
+    //    return shortest;
+    //}
+
+    //private List<NodeConnection> DistanceToConnection(Node from, NodeConnection currentConnection, Node end)
+    //{
+    //    if (!connections.Any(c => c != currentConnection))
+    //    {
+    //        Debug.Log("Dead end");
+    //        return null;
+    //    }
+
+    //    if (currentConnection.OtherNode(from) == end)
+    //    {
+    //        return path;
+    //    }
+
+    //    float min = Mathf.Infinity;
+
+    //    foreach (var connection in connections.Where(c => c.node1 == from || c.node2 == from))
+    //    {
+    //        DistanceToConnection(currentConnection.OtherNode(from), connection, end);
+    //    }
+
+    //    return distance + min;
+    //}
 }
